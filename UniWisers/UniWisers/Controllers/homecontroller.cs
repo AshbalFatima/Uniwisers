ca9d9b1421d5c89669594b35a825ff156795f25e
Ashbal <ashbalfatima@gmail.com>
2022-08-13 16:15:57 +0500

    Created Empty Project

---
+++ UniWisers/UniWisers/Controllers/homecontroller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniWisers.Controllers
+{
+    public class homecontroller : Controller
+    {
+        public string Index() {
+            return "AoA Pakistan";
+        }
+        public string Student () {
+            return "AOA Students";
+        }
+    }
+}
